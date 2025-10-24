d9cfc2164d4257f536209e0a4f7a6f8ad56d9d6c
lugosieben <62535192+lugosieben@users.noreply.github.com>
2025-10-24 18:05:31 +0200

    performance improvement

--- src/main/java/net/lugo/lightoverlay/util/OverlayChecker.java
+++ src/main/java/net/lugo/lightoverlay/util/OverlayChecker.java
@@ -7,46 +7,46 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 
-import java.util.Arrays;
+import java.util.Set;
 
 public class OverlayChecker {
     private static final MinecraftClient MC = MinecraftClient.getInstance();
-    private static final Block[] forbiddenBlocks = new Block[]{
+    private static final Set<Block> forbiddenBlocks = Set.of(
             Blocks.BEDROCK,
             Blocks.COMMAND_BLOCK,
             Blocks.CHAIN_COMMAND_BLOCK,
-            Blocks.REPEATING_COMMAND_BLOCK,
-    };
-    private static final Block[] topSolidExceptions = new Block[]{
+            Blocks.REPEATING_COMMAND_BLOCK
+    );
+    private static final Set<Block> topSolidExceptions = Set.of(
             Blocks.SLIME_BLOCK,
             Blocks.MUD,
-            Blocks.SOUL_SAND,
-    };
-    private static final Block[] specialSpawnConditionBlocks = new Block[]{
+            Blocks.SOUL_SAND
+    );
+    private static final Set<Block> specialSpawnConditionBlocks = Set.of(
             Blocks.RAIL,                // Only aquatic mobs
             Blocks.ACTIVATOR_RAIL,
             Blocks.DETECTOR_RAIL,
             Blocks.POWERED_RAIL,
             Blocks.WITHER_ROSE,         // Only wither skeletons
-            Blocks.SWEET_BERRY_BUSH,    // Only foxes
-    };
+            Blocks.SWEET_BERRY_BUSH    // Only foxes
+    );
 
 
     public static boolean shouldRenderOverlay(BlockPos pos) {
         //noinspection DataFlowIssue
         boolean isTopSolid = MC.world.isTopSolid(pos, MC.player);
-        boolean isTopSolidException = Arrays.asList(topSolidExceptions).contains(MC.world.getBlockState(pos).getBlock());
+        boolean isTopSolidException = topSolidExceptions.contains(MC.world.getBlockState(pos).getBlock());
         if (isTopSolidException) isTopSolid = true;
         BlockPos above = pos.up();
         boolean aboveTopSolid = MC.world.isDirectionSolid(above, MC.player, Direction.DOWN);
         if (!isTopSolid || aboveTopSolid) return false;
 
-        boolean isForbiddenBlock = Arrays.asList(forbiddenBlocks).contains(MC.world.getBlockState(pos).getBlock());
+        boolean isForbiddenBlock = forbiddenBlocks.contains(MC.world.getBlockState(pos).getBlock());
         boolean hideBecauseWater = ModConfig.hideWater && MC.world.isWater(above);
         boolean hideBecauseTransparent = ModConfig.hideTransparent && !MC.world.getBlockState(pos).isOpaque();
         if (isTopSolidException) hideBecauseTransparent = false;
         boolean hideBecauseSpecialSpawnCondition = !ModConfig.showSpecialSpawningConditionBlocks &&
-                Arrays.asList(specialSpawnConditionBlocks).contains(MC.world.getBlockState(above).getBlock());
+                specialSpawnConditionBlocks.contains(MC.world.getBlockState(above).getBlock());
         return !(isForbiddenBlock || hideBecauseWater || hideBecauseTransparent || hideBecauseSpecialSpawnCondition);
     }
 }
